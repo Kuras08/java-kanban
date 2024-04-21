d904b7c66cebcb097c4683212e43af91084c8f9f
Евгений <kurasovevgeni@yandex.ru>
2024-04-21 17:05:02 +0300

    5тз на проверку

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -2,15 +2,17 @@ package service;
 
 import model.Task;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final List<Task> browsingHistory = new ArrayList<>();
+    private static final int MAX_HISTORY_SIZE = 10;
+    private final List<Task> browsingHistory = new LinkedList<>();
+
 
     @Override
     public void add(Task task) {
-        if (browsingHistory.size() == 10) {
+        if (browsingHistory.size() == MAX_HISTORY_SIZE) {
             browsingHistory.removeFirst();
         }
         browsingHistory.add(task);
