28ffab2fd178c34d2bd759597a186850ddec0180
Евгений <kurasovevgeni@yandex.ru>
2024-05-12 06:05:49 +0300

    changed HistoryManager

--- src/Main.java
+++ src/Main.java
@@ -9,32 +9,32 @@ public class Main {
     public static void main(String[] args) {
 
         TaskManager manager = Managers.getDefault();
-        TaskStatus NEW = TaskStatus.NEW;
-        TaskStatus IN_PROGRESS = TaskStatus.IN_PROGRESS;
-        TaskStatus DONE = TaskStatus.DONE;
+        TaskStatus aNew = TaskStatus.NEW;
+        TaskStatus inProgress = TaskStatus.IN_PROGRESS;
+        TaskStatus done = TaskStatus.DONE;
 
-        Task task1 = manager.createTask(new Task("Task1", "Description", NEW));
+        Task task1 = manager.createTask(new Task("Task1", "Description", aNew));
         int idTask1 = task1.getId();
 
-        Task task2 = manager.createTask(new Task("Task2", "Description", IN_PROGRESS));
+        Task task2 = manager.createTask(new Task("Task2", "Description", inProgress));
         int idTask2 = task2.getId();
 
         Epic epic1 = manager.createEpic(new Epic("Epic1", "Description"));
         int idEpic1 = epic1.getId();
 
         Subtask subtask1ForEpic1 = manager.createSubtask(new Subtask("Subtask1ForEpic1",
-                "Description", DONE, idEpic1));
+                "Description", done, idEpic1));
         int idSubtask1ForEpic1 = subtask1ForEpic1.getId();
 
         Subtask subtask2ForEpic1 = manager.createSubtask(new Subtask("Subtask2ForEpic1",
-                "Description", NEW, idEpic1));
+                "Description", aNew, idEpic1));
         int idSubtask2ForEpic1 = subtask2ForEpic1.getId();
 
         Epic epic2 = manager.createEpic(new Epic("Epic2", "Description"));
         int idEpic2 = epic2.getId();
 
         manager.createSubtask(new Subtask("Subtask1ForEpic2", "Description",
-                IN_PROGRESS, idEpic2));
+                inProgress, idEpic2));
 
         /*System.out.println("Tasks:\n" + manager.getAllTasks() + "\n");
         System.out.println("Epics:\n" + manager.getAllEpics() + "\n");
